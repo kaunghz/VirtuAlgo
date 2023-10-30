ae9b2b5ffe527fa81a28272b755bb7ee0ef808e6
Bunniiez <vivienho03@gmail.com>
2023-10-30 03:58:59 -0400

    add table users and portfolio

--- setup.sql
+++ setup.sql
@@ -1,5 +1,14 @@
-CREATE DATABASE virtuAlgo;
-\c virtuAlgo
-CREATE TABLE virtu (
-	id SERIAL PRIMARY KEY,
+CREATE DATABASE virtualgo;
+\c virtualgo
+CREATE TABLE Users (
+	userId SERIAL PRIMARY KEY,
+	username VARCHAR(25),
+	saltedPass VARCHAR(50)
 );
+
+CREATE TABLE Portfolio (
+	portfolioId SERIAL PRIMARY KEY,
+	userId INTEGER REFERENCES Users(userId),
+	balance DECIMAL(10, 2)
+);
+
