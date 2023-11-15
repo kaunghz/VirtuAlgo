f65a98af1ea9667559163799d61099f4bdab2b15
Kaung Zan <khz24@drexel.edu>
2023-11-15 15:00:41 -0500

    updated chart and events

--- app/public/index.js
+++ app/public/index.js
@@ -1,7 +1,40 @@
 let stockSearch = document.getElementById("stockSearch");
 let stockSearchButton = document.getElementById("stockSearchButton");
 let errorMsg = document.getElementById("ErrorMsg");
+let balanceBlock = document.getElementById('balance');
+let balance = 100000;
 let intervalID;
+// Dynamic Components
+let valueDisplay;
+let chartContainer;
+let canvas;
+let buyStockButton;
+let sellStockButton;
+let buySellEventAdded = false;
+// initial state
+balanceBlock.textContent = "Balance: $" + balance.toFixed(2);
+
+stockSearchButton.addEventListener("click", () => {
+    var loading = document.createElement("span");
+    loading.textContent = "Loading...";
+    loading.style.marginTop = "10px";
+    document.body.appendChild(loading);
+    let ticker = stockSearch.value;
+    if (intervalID) {
+        clearInterval(intervalID);
+    }
+    intervalID = setInterval(function() {
+        balanceBlock.textContent = "Balance: $" + balance.toFixed(2);
+        loading.remove();
+        fetchStock(ticker);
+    }, 2000);
+});
+
+stockSearch.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        stockSearchButton.click();
+    }
+});
 
 function fetchStock(ticker) {
     fetch(
@@ -52,15 +85,18 @@ function makeChart(stocks, symbol) {
         existingChart.destroy();
     }
     // dynamically create the chart
-    var chartContainer = document.createElement("div");
-    chartContainer.style.width = "800px";
-    chartContainer.style.height = "400px";
-    chartContainer.id = "chartContainer";
-    var canvas = document.createElement("canvas");
-    canvas.id = "stockChart";
-    chartContainer.appendChild(canvas);
-    document.body.appendChild(chartContainer);
-
+    if (!chartContainer) {
+        chartContainer = document.createElement("div");
+        chartContainer.style.width = "800px";
+        chartContainer.style.height = "400px";
+        chartContainer.id = "chartContainer";
+    }
+    if (!canvas) {
+        canvas = document.createElement("canvas");
+        canvas.id = "stockChart";
+        chartContainer.appendChild(canvas);
+        document.body.appendChild(chartContainer);
+    }
     var labels = generateTimeLabels(stocks.length, 1);
 
     var curStock = stocks[0];
@@ -91,83 +127,87 @@ function makeChart(stocks, symbol) {
         "Low": curStock.LowPrice,
         "Current": curStock.ClosePrice
     }
-    console.log (curPricesDict);
     displayCurrentStockPrice(curPricesDict);
-    makeBuySellButtons();
+    makeBuySellButtons(curStock.ClosePrice);
 }
 
-stockSearchButton.addEventListener("click", () => {
-    let ticker = stockSearch.value;
-    if (intervalID) {
-        clearInterval(intervalID);
-    }
-    intervalID = setInterval(function() {
-        fetchStock(ticker);
-    }, 2000);
-});
-
 function displayCurrentStockPrice(curPrices) {
-    // BUG -------------- it does not update
-    for (var key in curPrices) {
-        var valueDisplay = document.createElement("span");
-        valueDisplay.textContent += (key + ": " + curPrices[key] + "\n");
+    // Check if valueDisplay already exists
+    let prices = ""
+    if (!valueDisplay) {
+        // If it doesn't exist, create a new span element
+        valueDisplay = document.createElement("span");
         valueDisplay.style.marginTop = "10px";
-        valueDisplay.style.fontWeight = "bold";
         document.body.appendChild(valueDisplay);
-        document.body.appendChild(valueDisplay);
-        document.body.appendChild(document.createElement("br"));
     }
+    // Update the content of valueDisplay
+    for (var key in curPrices) {
+        prices += key + ": " + curPrices[key] + "<br>";
+    }
+    valueDisplay.innerHTML = prices
 }
 
 // Buy and Sell Skeleton
-function makeBuySellButtons() {
+function makeBuySellButtons(curPrice) {
     // Create buttons dynamically
-    var buyStockButton = document.createElement("button");
-    buyStockButton.textContent = "Buy Stock";
-    buyStockButton.addEventListener("click", function() {
-        console.log("buy clicked");
-        buy();
-    });
-    var sellStockButton = document.createElement("button");
-    sellStockButton.textContent = "Sell Stock";
-    sellStockButton.addEventListener("click", function() {
-        console.log("sell clicked");
-        sell();
-    });
-    document.body.appendChild(buyStockButton);
-    document.body.appendChild(document.createElement("br"));
-    document.body.appendChild(sellStockButton);
+    if (!buyStockButton) {
+        buyStockButton = document.createElement("button");
+        buyStockButton.textContent = "Buy Stock";
+        document.body.appendChild(buyStockButton);
+        document.body.appendChild(document.createElement("br"));
+    }
+    if (!sellStockButton) {
+        sellStockButton = document.createElement("button");
+        sellStockButton.textContent = "Sell Stock";
+        document.body.appendChild(sellStockButton);
+        document.body.appendChild(document.createElement("br"));
+    }
+    if (!buySellEventAdded) {
+        buySellEventAdded = true;
+        buyStockButton.addEventListener("click", buyHandler);
+        sellStockButton.addEventListener("click", sellHandler);
+    }
+    // THIS HAS A BUG ------------- does not get the latest price
+    function buyHandler() {
+        buy(curPrice);
+    }
+    function sellHandler() {
+        sell(curPrice);
+    }
 }
 
 // buy and sell backend
-function buy() {
-  fetch("/buy-stock", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({stockName: "APPL", amount: 45.56, username: "test", portfolioName: "port1"}),
-  }).then(response => {
-    console.log("Status:", response.status);
-  }).then(body => {
-    console.log("Body:", body);
-  }).catch(error => {
-    console.log(error);
-  });
+function buy(curPrice) {
+    console.log(curPrice, "------------------", balance, "==========", balance - curPrice)
+    balance -= curPrice;
+    fetch("/buy-stock", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({stockName: "APPL", amount: 45.56, username: "test", portfolioName: "port1"}),
+    }).then(response => {
+        console.log("Status:", response.status);
+    }).then(body => {
+        console.log("Body:", body);
+    }).catch(error => {
+        console.log(error);
+    });
 };
 
-function sell() {
-  fetch("/sell-stock", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({stockName: "APPL", amount: 45.56, username: "test", portfolioName: "port1"}),
-  }).then(response => {
-    console.log("Status:", response.status);
-  }).then(body => {
-    console.log("Body:", body);
-  }).catch(error => {
-    console.log(error);
-  });
+function sell(curPrice) {
+    balance += curPrice;
+    fetch("/sell-stock", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({stockName: "APPL", amount: 45.56, username: "test", portfolioName: "port1"}),
+    }).then(response => {
+        console.log("Status:", response.status);
+    }).then(body => {
+        console.log("Body:", body);
+    }).catch(error => {
+        console.log(error);
+    });
 };
